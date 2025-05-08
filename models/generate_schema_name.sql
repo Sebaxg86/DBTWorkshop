da2a7f5a87e4cf232c1e1c4e9df9f3791797561e
Sebastian Chairez <sebas4856712@gmail.com>
2025-05-08 23:34:37 +0000

    generate_schema macro UPDATED

--- models/generate_schema_name.sql
+++ models/generate_schema_name.sql
@@ -1,3 +1,8 @@
 {% macro generate_schema_name(custom_schema, node) %}
-    
+    {% set default_schema=target.name %}
+    {% if target.name == 'prod' and custom_schema_name is not none %}
+        {{custom_schema_name | trim}}
+    {% else %}
+        default_schema
+    {% endif %}
 {% endmacro %}
